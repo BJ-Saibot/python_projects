e2d546f19b43bce1c63ca9efc9eb5e8d6ea8a7fb
BJ-Saibot <saibot.nur@gmail.com>
2020-07-24 02:11:49 +0300

    Add user_age.py file

---
+++ VK API/user_age.py
@@ -0,0 +1,7 @@
+def get_friends(user_id, fields):
+    """ Returns a list of user IDs or detailed information about a user's friends """
+    assert isinstance(user_id, int), "user_id must be positive integer"
+    assert isinstance(fields, str), "fields must be string"
+    assert user_id > 0, "user_id must be positive integer"
+
+    pass
